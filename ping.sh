642696662d78e740be36b3b5cf3dcffdfd55e802
刘永庆 <liuyongqing@liuyongqingdeMacBook-Air.local>
2016-01-06 20:39:10 +0800

    修改ping.sh 乱码的问题

--- ping.sh
+++ ping.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
+rm -rf ~/test.txt
 for i in `cat ~/ip.txt` ; do
 loss=`ping -c 3 $i|grep packet\ loss|awk -F, '{print $3}'|awk -F" " '{print $1}'`
 avg=`ping -c 3 $i|tail -1|awk -F/ '{print $5}'`
 tiao=`traceroute $i|wc -l`
-echo "$i 的丢包率为${loss} 平均ping值 ${avg} tracert跳数${tiao} ">~/test.txt
+echo "$i packet loss is ${loss} avg ping is  ${avg} traceroute is ${tiao} ">~/test.txt
 done
